1f9900b541cceb2971c1b3def3e4a114c04c2c43
Victor Martins <victor2007azevedo@hotmail.com>
2025-10-11 18:03:26 -0300

    feat: componente de moeda do home e ajuste no layout

---
+++ app/components/loan/coins-border.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center my-8">
+    <hr class="w-full border-white" />
+    <UIcon
+      name="i-lucide-coins"
+      class="mx-2 flex-shrink-0 text-white"
+      :size="20"
+    />
+    <hr class="w-full border-white" />
+  </div>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,16 @@
 <template>
-  <header>header</header>
-  <main>
-    <slot />
+  <header class="max-w-xl mx-auto p-4">
+    <nav>
+      <ul class="flex justify-end gap-4">
+        <li>
+          <UButton icon="i-lucide-log-out" color="neutral" size="md" />
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main class="max-w-xl mx-auto p-4 min-h-[calc(100vh-4rem)] flex">
+    <div class="flex-col relative w-full pb-[72px]">
+      <slot />
+    </div>
   </main>
-  <footer>footer</footer>
 </template>
