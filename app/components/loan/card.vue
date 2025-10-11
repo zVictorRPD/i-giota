77dbe9b2bbbd1fe11a45259361aa8f5d673a2ac2
Victor Martins <victor2007azevedo@hotmail.com>
2025-10-11 18:00:52 -0300

    feat: layout home

---
+++ app/components/loan/card.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full p-4 rounded-md bg-blue-950">
+    <div class="flex justify-between gap-2">
+      <slot />
+    </div>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,26 @@
+<template>
+  <div class="px-4">
+    <h6>Total</h6>
+    <h2 class="text-4xl font-bold">R$ 20.000,00</h2>
+  </div>
+  <LoanCoinsBorder />
+  <div class="grid gap-2.5">
+    <NuxtLink to="/1">
+      <LoanHomeCard />
+    </NuxtLink>
+  </div>
+  <UButton
+    icon="i-lucide-plus"
+    color="neutral"
+    class="absolute p-4 bottom-0 right-0"
+    size="xl"
+    @click="openAddLoanModal"
+  >
+    <span class="sr-only"> Adicionar Empréstimo </span>
+  </UButton>
+  <LoanAddModal />
+</template>
 <script setup lang="ts">
 const { data } = await useFetch("/api/loan/list");
+const { openAddLoanModal } = useLoanStore();
 </script>
-
-<template>
-  index page
-</template>
