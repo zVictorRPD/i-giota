ec0825b9ddec9d0337c5132a81374d90fe8cd5b0
Victor Martins <victor2007azevedo@hotmail.com>
2025-10-11 18:04:14 -0300

    feat: layout da tela de visualização

---
+++ app/pages/[slot].vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="px-4">
+    <h2 class="text-4xl font-bold mb-4">Nome</h2>
+    <div class="grid gap-2 grid-cols-4">
+      <div class="grid">
+        <h6>Crédito</h6>
+        <p>R$ 5.000</p>
+      </div>
+      <div class="grid">
+        <h6>Valor total</h6>
+        <p>R$ 6.000</p>
+      </div>
+      <div class="grid">
+        <h6>Juros</h6>
+        <p>5.25 a.m</p>
+      </div>
+      <div class="grid">
+        <h6>Pago</h6>
+        <p>05/12</p>
+      </div>
+    </div>
+  </div>
+  <LoanCoinsBorder />
+  <LoanParcelCard />
+</template>
