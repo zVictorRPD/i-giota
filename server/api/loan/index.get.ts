896198165a5f17da019b13f2811e0e38547c5b58
Felps <luisfelipeamorim@hotmail.com>
2025-10-12 21:10:59 -0300

    feat(loans): busca detalhada do empréstimo

--- server/api/loan/index.get.ts
+++ server/api/loan/index.get.ts
@@ -1,4 +1,23 @@
-export default defineEventHandler((event) => {
-  console.log("GET /api/loan");
-  return { message: "Get loan" };
+import { and, eq } from "drizzle-orm";
+import { db } from "~/db";
+import { loans, parcels } from "~/db/schema";
+
+export default defineEventHandler( async (event) => {
+  const userId = JSON.parse(parseCookies(event).user || "{}").id;
+  console.log("User ID:", event.context);
+  try {
+    const query = getQuery(event)
+    const loanId = query.id as string;
+  if (!loanId) {
+    return { success: false, error: "Loan ID is required" };
+  }
+  const loan = await db.select().from(loans).where(and(eq(loans.id, Number(loanId)), eq(loans.userId, userId))).get();
+  const parcelsFromLoan = await db.select().from(parcels).where(eq(parcels.loanId, Number(loanId)));
+  const receivedValue = parcelsFromLoan.reduce((acc, parcel) => acc + parcel.paidValue, 0);
+  const totalToReceive = parcelsFromLoan.reduce((acc, parcel) => acc + parcel.value, 0);
+  return { success: true, loan, totalToReceive, receivedValue, parcels: parcelsFromLoan };
+} catch (error) {
+  console.error("Error getting loan:", error);
+  return { success: false, error };
+}
 });
